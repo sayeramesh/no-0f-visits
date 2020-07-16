416ce0ad00f6542c29ddfa4c78d16d0066a58e92
Ramesh babu saye <43689480+sayeramesh@users.noreply.github.com>
2020-07-16 21:03:02 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+//libraries
+const express = require("express");
+const redis = require("redis");
+const process = require("process");
+
+//creating instances
+const app=express();
+
+const client=redis.createClient({
+    host:'redis-server'
+})
+client.set('visits',0);
+app.get('/',(req, res) => {
+    
+    client.get('visits', (err, visits)=>{
+        res.send("No of visits is "+visits);
+    
+        client.set("visits", parseInt(visits)+1);
+    
+    });
+    });
+    //callback function
+    app.listen(8081, ()=>{
+    
+        console.log('listening to port 8081');
+    });
